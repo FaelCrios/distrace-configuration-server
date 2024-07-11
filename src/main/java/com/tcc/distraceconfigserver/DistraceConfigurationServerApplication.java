9b852551cb5b27bd228f96e4f6cff1622269b284
Rafael <faelnek@gmail.com>
2024-07-11 10:49:08 -0300

    Finalização do projeto

---
+++ src/main/java/com/tcc/distraceconfigserver/DistraceConfigurationServerApplication.java
@@ -0,0 +1,16 @@
+package com.tcc.distraceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class DistraceConfigurationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistraceConfigurationServerApplication.class, args);
+    }
+
+}
